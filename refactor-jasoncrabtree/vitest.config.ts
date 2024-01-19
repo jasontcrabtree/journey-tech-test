7cc68bf12aeedfe70a1410c72fb68e2b485ceaf1
jasontcrabtree <jasontcrabtree@gmail.com>
2024-01-19 15:39:24 +1300

    update vitest include/exclude statement

--- refactor-jasoncrabtree/vitest.config.ts
+++ refactor-jasoncrabtree/vitest.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
     coverage: {
       include: ['src/*'],
       exclude: [
+        // TS files are imported into react components and tested there, or via static type checking (e.g. types.ts)
+        'src/**/*.ts',
         //   We exclude standard NextJS core pages as they aren't running any custom logic
         'src/pages/_app.tsx',
         'src/pages/_document.tsx',
